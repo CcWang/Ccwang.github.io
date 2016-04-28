afed7f523ad9ad5e3917d090f1d07d7772087b5b
Chongchong Wang <chongchong1110@gmail.com>
2016-04-27 21:55:42 -0700

    me page finish

--- static/app.js
+++ static/app.js
@@ -1,1 +1,24 @@
 var myApp = angular.module('myApp',['br.fullpage']);
+
+myApp.factory('mainFactory',function () {
+	// body...
+	var factory = {}
+	
+	return factory;
+})
+
+myApp.controller('mainController',function ($scope, mainFactory) {
+	// body...
+	var intro = "Full Stack Web Developer"
+	$scope.intro ='';
+
+	var i=0;
+	var timer = setInterval(function(){
+	 if(i<intro.length){
+	  	$scope.intro += intro[i];
+	     i++; 
+	   }
+		 $scope.$apply();
+	},300)
+	
+})
