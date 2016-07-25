d248dc055d1fb2105081189b28fd1428c5822b5a
CC Wang <chongchong1110@gmail.com>
2016-07-25 15:47:24 -0700

    changes about section

--- static/script.js
+++ static/script.js
@@ -29,18 +29,17 @@ angular.module('myApp', [])
       var timer = setInterval(function(){
         if(i<intro.length){
           $scope.intro += intro[i];
-          i++; 
+          i++;
         }
         $scope.$apply();
       },150)
-
-    $scope.about_1 = "I am a full-stack web developer pursuing exciting new opportunities, and proud to proclaim that I recently accomplished an immersive 14 week programming bootcamp, Coding Dojo. At Coding Dojo, I earned the highest earning of achievement, a Double Black Belt, which means that I mastered 3 full stacks: LAMP(Linux, Apache, MAMP, PHP), MEAN (MongoDB, Express, Angular, NodeJS) and Ruby on Rails."
+    $scope.about_1 ='A full-stack web developer pursuing exciting new opportunities.'
+    $scope.about_2= 'Have great passion on creating bugs. '
+    $scope.about_3="With the ability to thrive in a fast-paced environment, willing to give 100% to help the team succeed"
+    // $scope.about_1 = "A full-stack web developer pursuing exciting new opportunities, and proud to proclaim that I recently accomplished an immersive 14 week programming bootcamp, Coding Dojo. Detail-oriented and analytical from previous accounting career. A quick learner with the ability to thrive in a fast-paced environment, willing to give 100% to help the team succeed.  At Coding Dojo, I earned the highest earning of achievement, a Double Black Belt, which means that I mastered 3 full stacks: LAMP(Linux, Apache, MAMP, PHP), MEAN (MongoDB, Express, Angular, NodeJS) and Ruby on Rails."
 // ;   $scope.about_2 ="Before attending Coding Dojo, I worked in accounting. From this, I became very detail-oriented and analytic, and intend to carry these valuable skills into a programming role. I am a quick learner with the ability to thrive in a fast-paced environment, and am willing to do whatever it takes to help my team succeed: front end, back end and everything in between. I am eager to continue learning and join your development team!";
     }
 
 
   ]);
 })(window.angular);
-
-
-
